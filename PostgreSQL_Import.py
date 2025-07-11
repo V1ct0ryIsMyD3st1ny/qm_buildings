e4098d7f3a699fba6068ded899e19fc2140601e3
W1ck3d <stefanmoser1991@gmail.com>
2025-07-11 09:31:15 +0200

    Merge remote-tracking branch 'origin/main'

---
+++ PostgreSQL_Import.py
@@ -0,0 +1,98 @@
+from typing import List
+from sqlalchemy import create_engine
+from sqlalchemy import insert, select
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import relationship
+from geoalchemy2.types import Geometry
+from geoalchemy2.shape import WKTElement
+import pandas as pd
+
+
+class Base(DeclarativeBase):
+    pass
+
+class LookupBuilding(Base):
+    __tablename__ = "lookup_buildings"
+
+    hauskey: Mapped[int] = mapped_column(primary_key=True)
+    location: Mapped[str] = mapped_column(Geometry(geometry_type="POINT", srid=2056))
+
+    search_buildings: Mapped[List["SearchBuilding"]] = relationship(back_populates="lookup_building")
+
+
+    def __repr__(self) -> str:
+        return f"Building(hauskey={self.hauskey})"
+    
+class SearchBuilding(Base):
+    __tablename__ = "search_buildings"
+
+    hauskey: Mapped[int] = mapped_column(primary_key=True)
+    location: Mapped[str] = mapped_column(Geometry(geometry_type="POINT", srid=2056))
+    lookup_hauskey: Mapped[int] = mapped_column(ForeignKey("lookup_buildings.hauskey"))
+
+    lookup_building: Mapped["LookupBuilding"] = relationship(back_populates="search_buildings")
+
+
+    def __repr__(self) -> str:
+        return f"Building(hauskey={self.hauskey})"
+    
+engine = create_engine("postgresql+psycopg2://postgres:Quickmail2025!@localhost:5432/Datenmanagement")
+Session = sessionmaker(engine)
+
+def setup_tables(engine):
+
+    # Make sure the table is dropped and recreated to match the ORM definition
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+
+def populate_buildings_qm(Session):
+
+    filename = r'C:\Users\QPAuser\OneDrive - Quickmail AG\Python Scripts\Test\Gebäudedaten_Quickmail.csv'
+
+    df = pd.read_csv(filename, sep=";", header=0, encoding='utf-8', low_memory=False)
+    df = df[['Hauskey','Geo-X Gebäude', 'Geo-Y Gebäude']]
+    df.rename(columns={'Hauskey': 'hauskey'}, inplace=True)
+    df['Geo-X Gebäude'] = df['Geo-X Gebäude'] + 2000000
+    df['Geo-Y Gebäude'] = df['Geo-Y Gebäude'] + 1000000
+
+    df['location'] = df.apply(
+        lambda row: WKTElement(f"POINT({row['Geo-X Gebäude']} {row['Geo-Y Gebäude']})", srid=2056),
+        axis=1
+    )
+
+    df = df[['hauskey', 'location']]
+
+    records = df.to_dict(orient='records')
+
+    with Session.begin() as session:
+        session.execute(insert(QMBuilding), records)
+
+    print('QM Import successful')
+
+def populate_buildings_post(Session):
+
+    filename = r'C:\Users\QPAuser\OneDrive - Quickmail AG\Python Scripts\Test\Postdaten_bereinigt.csv'
+
+    df = pd.read_csv(filename, sep=";", header=0, encoding='utf-8', low_memory=False)
+    df = df[['HAUSKEY','KOORD_X_LV95', 'KOORD_Y_LV95']]
+    df.rename(columns={'HAUSKEY': 'hauskey'}, inplace=True)
+
+    df['location'] = df.apply(
+        lambda row: WKTElement(f"POINT({row['KOORD_Y_LV95']} {row['KOORD_X_LV95']})", srid=2056),
+        axis=1
+    )
+
+    df = df[['hauskey', 'location']]
+
+    records = df.to_dict(orient='records')
+
+    with Session.begin() as session:
+        session.execute(insert(PostBuilding), records)
+
+    print('Post Import successful')
+
+# setup_tables(engine)
+# populate_buildings_qm(Session)
+# populate_buildings_post(Session)
